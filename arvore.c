d47b2008f41c4f5789c3bf8c41d417de83c7063c
hoborox2 <43296441+hoborox2@users.noreply.github.com>
2018-10-27 10:14:53 -0300

    Update arvore.c
    Pronto xD

--- arvore.c
+++ arvore.c
@@ -9,13 +9,48 @@ struct _arvore {
 };
 
 Arvore* arvore_inserir(Arvore *a, Candidato cand) {
-  //Implemente
+	if(a == NULL) {
+		Arvore *novo = (Arvore*) malloc(sizeof(Arvore));
+		novo->cand = cand;
+		novo->esquerda = NULL;
+		novo->direita = NULL;
+		return novo;
+	}
+	if(cand.numero < a->cand.numero) {
+		a->esquerda = arvore_inserir(a->esquerda, cand);
+	} else {
+		a->direita = arvore_inserir(a->direita, cand);
+}
+return a;
 }
 
 Candidato* arvore_buscar(Arvore *a, int candNumero) {
-  //Implemente
+	if(a == NULL) 
+	{
+		return NULL;
+	}
+	if(a->cand.numero == candNumero)
+	{
+		return &a->cand;
+	}
+	if(candNumero < a->cand.numero) 
+	{
+		return arvore_buscar(a->esquerda, candNumero);
+	} 
+	else 
+	{
+		return arvore_buscar(a->direita, candNumero);
+	}
 }
 
 void arvore_imprime_em_ordem(Arvore *a) {
-  //Implemente
+	if(a->esquerda != NULL) 
+	{
+		arvore_imprime_em_ordem(a->esquerda);
+	}
+	printf("%lf ", a->cand);
+	if(a->direita != NULL) 
+	{
+		arvore_imprime_em_ordem(a->direita);
+	}
 }
